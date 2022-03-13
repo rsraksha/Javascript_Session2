2d8b5319f2a50eb86bbcae60afec15fb6155acd7
R S Raksha <rsrakshars1@gmail.com>
2022-03-13 14:48:49 +0530

    added

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+var str = "Skill Safari"
+
+//1) To find length of string
+console.log("\nLength of the given string = " +str.length)
+
+//2) Convert letters of the string into capital
+console.log("\nUpperCase of the given string = " +str.toUpperCase())
+
+//3) To find index position of a
+console.log("\nIndex position of \"a\" is = ",+str.indexOf("a"))
+
+//Conversation between Captain America and Iron Man in Avengers
+console.log("\nConversation between Captain America and Iron Man in Avengers -- \n\nIron Man : [as the fight begins]  Call it, Captain! \n\nCaptain America : Alright, listen up. Until we can close that portal, our priority's containment. Barton, I want you on that roof, eyes on everything. Call out patterns and strays. Stark, you got the perimeter. Anything gets more than three blocks out, you turn it back or you turn it to ash. \n\nIron Man : Right. Better clench up, Legolas. \nCaptain America : Thor, you gotta try and bottleneck that portal. Slow 'em down. You got the lightning. Light the bastards up.\n\nCaptain America : You and me, we stay here on the ground, keep the fighting here. And Hulk? \n\nCaptain America : Smash!")
+
+var height = 5.1
+var weight = 60
+var BMI = weight / height *2
+console.log("\nYour Body Mass Index is: ",+BMI)
+
+
+

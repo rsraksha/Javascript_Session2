dc881b18b4d9ec9c2f8db156a55d7c5f109f90e8
R S Raksha <rsrakshars1@gmail.com>
2022-03-13 14:50:07 +0530

    comment

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ console.log("\nIndex position of \"a\" is = ",+str.indexOf("a"))
 //Conversation between Captain America and Iron Man in Avengers
 console.log("\nConversation between Captain America and Iron Man in Avengers -- \n\nIron Man : [as the fight begins]  Call it, Captain! \n\nCaptain America : Alright, listen up. Until we can close that portal, our priority's containment. Barton, I want you on that roof, eyes on everything. Call out patterns and strays. Stark, you got the perimeter. Anything gets more than three blocks out, you turn it back or you turn it to ash. \n\nIron Man : Right. Better clench up, Legolas. \nCaptain America : Thor, you gotta try and bottleneck that portal. Slow 'em down. You got the lightning. Light the bastards up.\n\nCaptain America : You and me, we stay here on the ground, keep the fighting here. And Hulk? \n\nCaptain America : Smash!")
 
+//Body Mass Index logic
 var height = 5.1
 var weight = 60
 var BMI = weight / height *2
